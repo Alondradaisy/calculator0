3c93ffee204bdac52d1580e302df176fde122025
Student <example@example.com>
2019-10-21 13:57:31 -0700

    add temp converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
+//
+// var divide = function(number1, number2) {
+//   return (number1/(number2*number2));
 // };
 //
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
-// alert(result);
+// var number1 = parseInt(prompt("Enter Weight (in lb):"));
+// var number2 = parseInt(prompt("Enter Height (in inches):"));
+// var result1 = divide(number1, number2);
+//
+// var result2 = result1*703
+// alert(result2);
 
 
-var divide = function(number1, number2) {
-  return (number1/(number2*number2));
-};
-
-var number1 = parseInt(prompt("Enter Weight (in lb):"));
-var number2 = parseInt(prompt("Enter Height (in inches):"));
-var result1 = divide(number1, number2);
-
-var result2 = result1*703
-alert(result2);
+// var fahrenheit = function(tempC) {
+//   return ((tempC*(9/5))+32);
+// };
+// var tempC = parseInt(prompt("Enter Temp in Celsius"))
+// var result = fahrenheit(tempC)
+// alert(result)
